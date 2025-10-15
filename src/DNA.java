29dbf375cd60053998e3614034d2740931b2bd06
emill-y <eishayadav@gmail.com>
2025-10-15 05:11:07 -0700

    haandle easy edge cases

--- src/DNA.java
+++ src/DNA.java
@@ -16,6 +16,12 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
+        // Handle Edge Cases
+        if (sequence.isEmpty() || sequence.length() < STR.length()){
+            return 0;
+        }
+
+        //
 
         return 0;
     }
