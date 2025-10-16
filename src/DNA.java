01caedb6ca487bf80553acf579f224e127079955
emill-y <eishayadav@gmail.com>
2025-10-16 16:06:52 -0700

    implemented initial

--- src/DNA.java
+++ src/DNA.java
@@ -21,8 +21,36 @@ public class DNA {
             return 0;
         }
 
-        //
-
-        return 0;
+        // Parse sequence to identify beginning of STR
+        // Continue to add to run-length as STR
+        // When STR has a mistake still keep known prior segment of STR
+        int strLen = STR.length();
+        int seqLen = sequence.length();
+        int[] seqRep = new int[seqLen];
+//        String[] STRsegments = new String[strLen];
+//        for(int i = 0; i < strLen; i++){
+//            STRsegments[i] = STR.substring(0,i);
+//        }
+//        int counter = 0;
+//        int numRepeats = 0;
+        int maxNumRepeats = 0;
+        for (int i = seqLen - strLen; i >= 0; i--) {
+            // I don't know if the region matching function is allowed
+            // Since it does do a comparision-- and I am not yet sure of the runtime of the comparison
+            if (sequence.regionMatches(i, STR, 0, strLen)) {
+                // Uses ternary operator to check if there is a continuing repeat
+                seqRep[i] = 1 + ((i + strLen < seqLen) ? seqRep[i + strLen] : 0);
+                // Update Maximum value
+                if (seqRep[i] > maxNumRepeats) maxNumRepeats = seqRep[i];
+            } else {
+                // Mark broken repeat
+                seqRep[i] = 0;
+            }
+        }
+        return maxNumRepeats;
     }
 }
+
+
+
+
